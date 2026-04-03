2347e4245be7db8386996d5278a4a75e6f23b39a
Meleki Idris <melekiolami00@gmail.com>
2026-04-03 07:10:06 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, File, UploadFile
+from utils import split_text, open_pdf, store_embeddings
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_openai import ChatOpenAI
+
+
+
+app = FastAPI()
+os.makedirs("data", exist_ok=True)
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+embeddings = OpenAIEmbeddings()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "server is running"}
+
+@app.post("/upload")
+def upload_file(file: UploadFile = File(...)):
+    safe_filename = os.path.basename(file.filename)
+    file_location = f"data/{safe_filename}"
+
+    contents = file.file.read()
+
+    if len(contents) > 5_000_000:
+        return {"error":"File too Large"}
+
+
+    with open(file_location, "wb") as f:
+        f.write(contents)
+
+    full_text = open_pdf(file_location)
+    chunks = split_text(full_text, chunk_size=3000)
+    store_embeddings(chunks, file_location)
+
+
+    return {"info": f"file saved at {file_location}",
+                "text_preview": full_text[:200],
+                "total_length": len(full_text),
+                "chunk_length": len(chunks),
+                "chunks": chunks[:5]
+                }
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+@app.post("/ask")
+def ask_question(question: str):
+    if not question:
+        return {"error": "question is requried"}
+    db = Chroma(
+        collection_name="documents", 
+        embedding_function=embeddings, 
+        persist_directory="db"
+        )
+    
+    results = db.similarity_search(question, k=3)
+    context = ""
+
+    for result in results:
+        context += result.page_content + "\n\n"
+    
+    response = llm.invoke(f"Answer the question based on this context:\n{context}\n\nQuestion: {question}")
+    return {"answer": response.content }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,51 @@
+import os
+from pypdf import PdfReader
+from dotenv import load_dotenv
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_chroma import Chroma
+
+
+
+load_dotenv()
+embeddings = OpenAIEmbeddings()
+api_key = os.getenv("OPENAI_API_KEY")
+
+
+
+
+def open_pdf(file_location):
+    reader = PdfReader(file_location)
+    full_text = ""
+
+    for page in reader.pages:
+        text = page.extract_text()
+        if text:
+            full_text += text + "/n"
+    return full_text
+
+
+def split_text(full_text, chunk_size=3000):
+    chunk = []
+    for i in range(0, len(full_text), chunk_size):
+        chunk.append(full_text[i:i+chunk_size])
+    return chunk
+
+
+
+
+def store_embeddings(chunks, file_location):
+    db = Chroma(
+        collection_name="documents", 
+        embedding_function=embeddings, 
+        persist_directory="db"
+        )
+
+    print(f"Number of chunks: {len(chunks)}")
+
+    db.add_texts(
+        chunks, 
+        metadatas=[{"source": file_location}] * len(chunks)
+        )
+
+    print("Embeddings stored successfully!")
+
